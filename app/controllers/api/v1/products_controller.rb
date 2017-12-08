f8c8ce2bff3d1f38b0796d88ebb10eacf2d06a00
Alejok28 <alejo_k28@hotmail.com>
2017-12-08 13:56:24 -0500

    Create a product and render in json format

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -10,9 +10,14 @@ module Api
       #   @product = Product.new
       # end
       #
-      # def create
-      #   @product = Product.create(product_params)
-      # end
+      def create
+        product = Product.new(product_params)
+        if product.save
+          render json: product
+        else
+          render json: { errors: product.errors }, status: 422
+        end
+      end
       #
       # def edit
       #   @product = Product.find(params[:id])
@@ -22,10 +27,10 @@ module Api
       #   @product = Product.update(params[:id], product_params)
       # end
 
-      # private
-      #   def product_params
-      #     params.require(:product).permit(:name, :price)
-      #   end
+      private
+        def product_params
+          params.require(:product).permit(:name, :price)
+        end
     end
 
   end
