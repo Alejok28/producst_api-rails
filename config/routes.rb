865a20551675fba3d1d4ba4b1586032cad3b2bd4
Alejok28 <alejo_k28@hotmail.com>
2017-12-08 13:58:41 -0500

    Add create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :articles, only: [:index, :create, :update, :destroy]
+      resources :products, only: [:index, :create, :update, :destroy]
     end
   end
 
